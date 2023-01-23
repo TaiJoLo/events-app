55081395244ed7de44fa6632f38d4674c47932c0
LOLO <lotaijo@gmail.com>
2023-01-24 08:08:26 +1300

    add if statement

--- src/pages/api/email-registration.js
+++ src/pages/api/email-registration.js
@@ -1,17 +1,30 @@
 import prisma from "../../../lib/prisma";
 
 export default async function test(req, res) {
+  const { method } = req;
   const { email, eventId, cityId } = req.body;
-  console.log("req.body_eamil_registration:", email, eventId);
-  const result = await prisma.emails.create({
-    data: {
-      email: email,
-      event_id: eventId,
-      city_id: cityId,
-    },
-  });
 
-  res.status(201).json(result, {
-    message: `You have been registered successfully with the email: ${email} for the event: ${eventId}`,
-  });
+  if (!eventId) {
+    return res.status(404).json({
+      status: 404,
+      message: "Events data not found",
+    });
+  }
+  if (method === "POST") {
+    if (!email | !email.includes("@")) {
+      res.status(422).json({ message: "Invalid email address" });
+    }
+
+    const result = await prisma.emails.create({
+      data: {
+        email: email,
+        event_id: eventId,
+        city_id: cityId,
+      },
+    });
+
+    res.status(201).json(result, {
+      message: `You have been registered successfully with the email: ${email} for the event: ${eventId}`,
+    });
+  }
 }
